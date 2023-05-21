a49c71c01a1141cea53428fff83ca71498dfcbbf
rexcyrio <stefancyrio@gmail.com>
2023-05-21 17:33:14 +0800

    Add JSDocs to api

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -4,8 +4,19 @@ declare global {
   interface Window {
     electron: ElectronAPI;
     api: {
+      /**
+       * Asynchronously resolves with a response from the main process.
+       */
       invoke: (channel: string, data: any) => Promise<any>;
+
+      /**
+       * Synchronously resolves with a response from the main process.
+       */
       sendSync: (channel: string, data: any) => any;
+
+      /**
+       * Sends an asynchronous message to the main process.
+       */
       send: (channel: string, data: any) => void;
     };
   }
