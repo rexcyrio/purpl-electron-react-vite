3cf264f36e565205c73808473f15954b504a19da
rexcyrio <stefancyrio@gmail.com>
2023-05-21 17:32:31 +0800

    Use eslint-plugin-react-hooks and fswin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "prettier"
