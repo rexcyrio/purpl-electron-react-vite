a152bd30802390f8443d8a5ed2dace0cadbddb0b
rexcyrio <stefancyrio@gmail.com>
2023-05-21 17:27:38 +0800

    Rename renderer entry point

--- src/renderer/src/renderer.tsx
+++ src/renderer/src/renderer.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 import "./index.css";
 import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
